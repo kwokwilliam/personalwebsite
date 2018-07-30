45da8c045349d1591183854b7d846bd91ab9abd7
kwokwilliam <wkwok16@uw.edu>
2018-07-30 10:04:11 -0700

    Added stuff

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
-import mainImg from '../assets/imgs/main/main.jpg';
+import mainImg from '../assets/imgs/main/main.jpg'; // TODO: make file smaller, prefetch img
+import './MainPage.css';
+import Fade from 'react-reveal/Fade'
 
 export default class MainPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loaded: false
+        }
+    }
     render() {
-        return <div>Main Page {this.props.mobile && 'mobile'}
-            <img src={mainImg} className={`${this.props.mobile ? 'mainimg' : 'mainimg-mobile'}`} /></div>
+        return <div style={{ display: 'flex' }}>
+            <Fade clear when={this.state.loaded}>
+                <img src={mainImg} onLoad={() => { this.setState({ loaded: true }); }} className={`mainimg-1 ${!this.props.mobile ? 'mainimg' : 'mainimg-mobile'}`} />
+            </Fade>
+        </div>
     }
 }
