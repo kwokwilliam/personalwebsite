8c302d8f6a9c7fba0582b8191ea8e9d67b620acc
kwokwilliam <wkwok16@uw.edu>
2018-08-07 18:59:50 -0700

    Added flexbox

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import mainImg from '../assets/imgs/main/main.jpg'; // TODO: make file smaller, prefetch img
 import './MainPage.css';
-import Fade from 'react-reveal/Fade'
+import Fade from 'react-reveal/Fade';
+import { Grid, Row, Col } from 'react-flexbox-grid';
 
 export default class MainPage extends Component {
     constructor(props) {
@@ -11,10 +12,17 @@ export default class MainPage extends Component {
         }
     }
     render() {
-        return <div style={{ display: 'flex' }}>
-            <Fade clear when={this.state.loaded}>
-                <img src={mainImg} onLoad={() => { this.setState({ loaded: true }); }} className={`mainimg-1 ${!this.props.mobile ? 'mainimg' : 'mainimg-mobile'}`} />
-            </Fade>
-        </div>
+        return <Grid fluid>
+            <Row>
+                <Col xs={12} md={6}>
+                    <Fade clear when={this.state.loaded}>
+                        <img src={mainImg} onLoad={() => { this.setState({ loaded: true }); }} className={`mainimg-1 ${!this.props.mobile ? 'mainimg' : 'mainimg-mobile'}`} />
+                    </Fade>
+                </Col>
+                <Col xs={12} md={6}><div>Content</div></Col>
+            </Row>
+
+            <div>fdsafdsa</div>
+        </Grid >
     }
 }
