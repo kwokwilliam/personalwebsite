602246f996c38c265f59f978ff33629ebf27a4ed
kwokwilliam <wkwok16@uw.edu>
2018-08-17 14:33:48 -0700

    added styles

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,22 +1,17 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 import Fade from 'react-reveal/Fade';
-import { Collapse, CardBody, Card, CardHeader, Input } from 'reactstrap';
 
-import { Button } from 'reactstrap';
 import { Grid, Row, Col } from '../../node_modules/react-flexbox-grid';
-import { PDFExport, savePDF } from '@progress/kendo-react-pdf';
+import { PDFExport } from '@progress/kendo-react-pdf';
+
+import { faDownload } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default class Projects extends Component {
     resume;
     constructor() {
         super();
         this.state = {
-            expanded: [],
-            po: poType.date, // DATE, TOOL
-            filterString: '',
-            highlighted: '',
-            dropdownOpen: false
         }
     }
 
@@ -24,19 +19,32 @@ export default class Projects extends Component {
         this.resume.save();
     }
 
+    inchToPixels = (inch) => {
+        return 72 * inch;
+    }
+
     render() {
-        let resumeObj = <PDFExport paperSize={'Letter'}
-            ref={(r) => this.resume = r}>
-            <div style={styles(this.props.mobile).paperStyle}>
-                fdsaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab
+        let resumeObj =
+            <PDFExport paperSize={'Letter'}
+                ref={(r) => this.resume = r}>
+                <div style={styles(this.props.mobile).paperStyle}> {/* Resume content starts in here */}
+                    fdsaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab
             </div>
-        </PDFExport>;
+            </PDFExport>;
         return <Grid fluid>
             <Row>
                 <div style={{ width: '100%' }}>
-                    <Fade right cascade>
-                        <button onClick={this.exportPDFWithMethod}>export</button>
-                        {resumeObj}
+                    <Fade clear cascade>
+                        <div>
+
+                            <div style={{ margin: 'auto', textAlign: 'center', marginBottom: 10 }}>
+                                This page doesn't appear correctly on mobile, sorry!<br /><br />
+                                <a onClick={this.exportPDFWithMethod} style={{ cursor: 'pointer', margin: 'auto', textDecoration: 'none', color: '#005696', minWidth: "60px", textAlign: 'center' }}>
+                                    <FontAwesomeIcon icon={faDownload} size={"2x"} /> Download PDF
+                                </a>
+                            </div>
+                            {resumeObj}
+                        </div>
                     </Fade>
                 </div>
             </Row>
@@ -50,40 +58,9 @@ const styles = (mobile) => {
             height: '792px',
             width: '612px',
             padding: 'none',
-            margin: 'none',
-            backgroundColor: 'red',
+            backgroundColor: 'white',
             boxShadow: '5px 5px 5px #888888',
             margin: 'auto'
-        },
-        courseworkList: {
-            color: '#005696',
-            border: '1px solid gray',
-            padding: '7px',
-            borderRadius: '5px',
-            background: '#efefef',
-            width: '100%',
-            boxShadow: '5px 5px 5px #dddddd',
-            height: '100%',
-            cursor: 'pointer'
-        },
-        headingText: {
-            fontSize: 20
-        },
-        quarterSegText: {
-            fontSize: 20,
-            marginLeft: '2%',
-            cursor: 'text',
-            color: 'black'
-        },
-        row: {
-            marginBottom: 20
         }
     }
-}
-
-const poType = {
-    date: "DATE",
-    dateRev: "DATE_REVERSE",
-    tool: "TOOL",
-    type: "TYPE"
 }
