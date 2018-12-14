abc130ab53bcd20387e251766a6bc9b8d4ba74cd
kwokwilliam <wkwok16@uw.edu>
2018-12-14 01:48:18 -0800

    added state stuff

--- src/components/Pages/TutorQ/Components/StudentLocation/StudentLocation.js
+++ src/components/Pages/TutorQ/Components/StudentLocation/StudentLocation.js
@@ -1,16 +1,16 @@
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircle } from '@fortawesome/free-regular-svg-icons';
 import TELabMap from './Assets/TELabMap.png'
 
 export default function StudentLocation({ location, setLocation, student }) {
-    const [imgPos, setImgPos] = useState({ x: 0, y: 0 });
-    const imgRef = useRef(null);
+    const [imgData, setImgData] = useState({ x: 0, y: 0 });
     return <div style={{ height: '60%' }}>
-        <img ref={imgRef} src={TELabMap} style={{ maxHeight: '50vh', maxWidth: '90vw' }} onClick={(e) => {
-            setImgPos({ x: e.target.offsetLeft, y: e.target.offsetTop });
-            console.log(e.target.offsetLeft, e.target.height);
+        <img src={TELabMap} style={{ maxHeight: '50vh', maxWidth: '90vw' }} onClick={(e) => {
+            let { offsetLeft, offsetTop, width, height } = e.target
+            setImgData({ x: offsetLeft, y: offsetTop, width, height });
+            // console.log(e.target.offsetLeft, e.target.height);
         }} />
-        <div style={{ position: 'absolute', left: imgPos.x, top: imgPos.y }}><FontAwesomeIcon icon={faCircle} /></div>
+        <div style={{ position: 'absolute', left: imgData.x, top: imgData.y }}><FontAwesomeIcon icon={faCircle} /></div>
     </div>
 }
