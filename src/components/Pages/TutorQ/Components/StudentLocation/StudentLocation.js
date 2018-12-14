ef67be56c92d61a3a8c7239aa96367dabd0b2a1b
kwokwilliam <wkwok16@uw.edu>
2018-12-14 01:10:02 -0800

    sdf

--- src/components/Pages/TutorQ/Components/StudentLocation/StudentLocation.js
+++ src/components/Pages/TutorQ/Components/StudentLocation/StudentLocation.js
@@ -1,9 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircle } from '@fortawesome/free-regular-svg-icons';
 import TELabMap from './Assets/TELabMap.png'
 
 export default function StudentLocation({ location, setLocation, student }) {
-
+    const [imgPos, setImgPos] = useState({ x: 0, y: 0 });
+    const imgRef = useRef(null);
     return <div style={{ height: '60%' }}>
-        <img src={TELabMap} style={{ maxHeight: '50vh', maxWidth: '90vw' }} />
+        <img ref={imgRef} src={TELabMap} style={{ maxHeight: '50vh', maxWidth: '90vw' }} onClick={(e) => {
+            setImgPos({ x: e.target.offsetLeft, y: e.target.offsetTop });
+            console.log(e.target.offsetLeft, e.target.height);
+        }} />
+        <div style={{ position: 'absolute', left: imgPos.x, top: imgPos.y }}><FontAwesomeIcon icon={faCircle} /></div>
     </div>
 }
