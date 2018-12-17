e20301d0a4cb56349d816c77dc780c10a969c960
kwokwilliam <wkwok16@uw.edu>
2018-12-16 23:04:43 -0800

    Added bundle analyzer script

--- src/components/Pages/TutorQ/Admin/TutorQAdmin.js
+++ src/components/Pages/TutorQ/Admin/TutorQAdmin.js
@@ -1,7 +1,53 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
+import { Input, Button, Alert } from 'reactstrap';
+import Loader from 'react-loader-spinner'
+import 'firebase/auth';
+
+
+let provider = new firebase.auth.GoogleAuthProvider();
 
 export default class TutorQAdmin extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            user: null,
+            loading: true
+        }
+    }
+
+    componentWillMount() {
+        firebase.auth().onAuthStateChanged(user => {
+            if (user) {
+                this.setState({ user, loading: false });
+            } else {
+                this.setState({ user: null, loading: false });
+            }
+        });
+    }
+
     render() {
-        return <div>Admin</div>
+        console.log(this.state.user);
+        return <div style={{ textAlign: 'center' }}>
+            <h1>
+                TutorQ Admin Panel
+            </h1>
+
+            {this.state.loading && <div><Loader
+                type="Oval"
+                color="#005696"
+                height="100"
+                width="100"
+            />   </div>}
+            <Button onClick={() => {
+                firebase.auth().signInWithRedirect(provider);
+            }}>Sign in with Google</Button>
+            <Button onClick={() => {
+                firebase.auth().signOut();
+            }}>Sign out</Button>
+            {this.state.user && <>Currently signed in</>}
+        </div>
     }
 }
+
+//       ".write": "auth != null && root.child('Users').child(auth.uid).child('permission').val() === 'author'"
